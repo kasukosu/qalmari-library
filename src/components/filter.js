b229211837561e0443c745e53e9f300d7fb676b2
Kasper Koskenvirta <kasper.koskenvirta@betta.fi>
2021-11-15 21:43:02 +0200

    css

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,14 +1,13 @@
 const Filter = ({newSearch, searchHandler, setSortMethod}) => {
   return (
-      <section className="filter">
+      <div className="filter">
           <h2 htmlFor="filter">Filter Books</h2>
           <input type="text" placeholder="Search..." name="filter" onChange={searchHandler} value={newSearch} />
           <div>
             <button onClick={() => setSortMethod(0)}>Sort by asc</button>
             <button onClick={() => setSortMethod(1)}>Sort by desc</button>
           </div>
-
-      </section>
+      </div>
   );
 }
 
